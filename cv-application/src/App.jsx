bbb9ee6e010dfea2cbcfda91baf000b37f123ccb
Farzaamm <93753345+Farzaamm@users.noreply.github.com>
2024-05-22 10:30:53 +0800

    components made

---
+++ cv-application/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import './components/Footer.jsx'
+import './components/Form.jsx'
+import './components/Navbar.jsx'
+import './components/Preview.jsx'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+      <Footer />
+      <Preview />
+      <h1>4dddd</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react-memory-card/react-memory-cards/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Card from './components/Card'
+import Footer from './components/Footer'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  )
+}
+
+export default App
